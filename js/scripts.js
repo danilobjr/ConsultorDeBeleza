76ecca7605d9f771638e063e7838254ee1c3c3aa
Danilo Jr. <danilobjr@gmail.com>
2013-07-18 19:57:59 -0300

    Implementando página de produtos

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,21 @@ $(function () {
 
     $('span.ano').text(new Date().getFullYear());
 
+    /* Slide PRODUTOS */
+
+    $('#slideProdutos').jcarousel({
+        wrap: 'circular',
+        visible: 5
+    });
+
+    $('.jcarousel-item').click(function (e) {
+        var produto = $(e.currentTarget);
+        var idProduto = produto.data().idProduto;
+
+        var display = $('.display');
+        var figuras = display.children().removeClass('active');
+
+        var figuraEscolhida = figuras.filter('[data-id-produto=' + idProduto + ']');
+        figuraEscolhida.addClass('active');
+    });
 });
